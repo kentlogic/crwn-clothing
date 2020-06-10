b66333893f616cf9289672cb5c425f41c2a69f84
kentlogic <iict.vincent@gmail.com>
2020-06-10 21:34:45 +0800

    Updated the shop page, collection-preview and collection-item components.

--- src/container/App.js
+++ src/container/App.js
@@ -2,30 +2,14 @@ import React from 'react';
 import './App.css';
 import Homepage from '../components/pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom'
-
-
-const HatsPage = () => (
-  <div>
-      <h1>Hats</h1>
-  </div>
-);
-
-
-const JacketsPage = () => (
-  <div>
-      {console.log('Jackets')}
-      <h1>Jackets</h1>
-  </div>
-);
-
+import ShopPage from '../components/pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/jackets' component={JacketsPage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

8994e75744582672aa5b2e4d9cb927ada0115a42
kentlogic <iict.vincent@gmail.com>
2020-06-10 21:52:02 +0800

    Added the header component with the logo and links to the home and shop pages.

--- src/container/App.js
+++ src/container/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Homepage from '../components/pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from '../components/pages/shop/shop.component';
+import Header from '../components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />

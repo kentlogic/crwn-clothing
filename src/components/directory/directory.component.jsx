a56b1a15904297685f610a83b54b462a3a1701db
kentlogic <iict.vincent@gmail.com>
2020-06-11 15:14:30 +0800

    Fixed typo errors

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import MenuItem from '../menu-item/menu-item.component';
+import SECTION_DATA from './sections.data';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: SECTION_DATA
+  	}
+  }
+
+  render() {
+    return (
+      <div className='directory-menu'>
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;

099d58c8e5b3fd161c716bfe4847e34484c3e381
kentlogic <iict.vincent@gmail.com>
2020-06-12 16:14:08 +0800

    Added cart-icon and cart-dropdown component. Implemented the cart-reducer.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,7 +5,7 @@ import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 const CartIcon = ({ toggleCartHidden }) => (
-    <div className='cart-icon' onClick={toggleCartHidden}>
+    <div className='cart-icon'  onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
         <span className='item-count'>0</span>
     </div>
